9f46ec095cd8b5d8678a2069be42750e04d5777e
Robert Hurley <robert-hurley@hotmail.com.au>
2025-10-07 18:01:03 +1100

    Update

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ if [ ! -n $HOME ]; then
 	setHomePath
 fi
 
-
 #Setup XDG variables
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_CACHE_HOME=$HOME/.cache
@@ -26,12 +25,16 @@ makeDirIfNotExist $XDG_DATA_HOME/fonts
 makeDirIfNotExist $HOME/.config/wget
 makeDirIfNotExist $XDG_DATA_HOME/nvm
 makeDirIfNotExist $XDG_STATE_HOME
-makeDirIfNotExist $XDG_CACHE_HOME/zsh
 makeDirIfNotExist $XDG_CACHE_HOME/starship
-makeDirIfNotExist $XDG_CONFIG_HOME/git
-makeDirIfNotExist $XDG_CONFIG_HOME/zsh
-makeDirIfNotExist $XDG_CONFIG_HOME/npm
+makeDirIfNotExist $XDG_CACHE_HOME/zsh
 makeDirIfNotExist $XDG_CONFIG_HOME/alacritty
+makeDirIfNotExist $XDG_CONFIG_HOME/i3
+makeDirIfNotExist $XDG_CONFIG_HOME/git
+makeDirIfNotExist $XDG_CONFIG_HOME/npm
+makeDirIfNotExist $XDG_CONFIG_HOME/polybar
+makeDirIfNotExist $XDG_CONFIG_HOME/picom
+makeDirIfNotExist $XDG_CONFIG_HOME/tmux
+makeDirIfNotExist $XDG_CONFIG_HOME/zsh
 continue?
 
 
@@ -41,29 +44,28 @@ sudo apt-get update && sudo apt-get upgrade -y
 continue?
 
 #Install Tools/Utilities
-sudo apt install git neovim tmux vlc curl eza mysql-server gnome-tweaks stow bear jq  -y
-sudo apt-get install ripgrep make gcc unzip unzip fzf
-curl -sS https://starship.rs/install.sh | sh
-sudo apt-get update && sudo apt-get upgrade -y
-continue?
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y \
+  git neovim tmux vlc curl sshfs eza gnome-tweaks stow bear jq ripgrep make gcc unzip fzf wget \
+  i3 i3status i3lock dmenu rofi picom feh xinit x11-xserver-utils xclip \
+  lxappearance papirus-icon-theme fonts-font-awesome fonts-jetbrains-mono fonts-firacode \
+  pulseaudio pavucontrol alsa-utils playerctl pamixer \
+  nitrogen neofetch polybar light arandr network-manager-gnome policykit-1-gnome xfce4-power-manager
+
+sudo apt install ffmpeg 7zip poppler-utils fd-find ripgrep zoxide imagemagick
 
 sudo snap install btop
+continue?
 
-#yazi
-apt install ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide imagemagick
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup update
-cargo install --locked yazi-fm yazi-cli
-
-
-
+# install Starship prompt
+curl -sS https://starship.rs/install.sh | sh
+continue?
 
 #Install misc deps
-	#Bear
 apt-get install python cmake pkg-config
 apt-get install libfmt-dev libspdlog-dev nlohmann-json3-dev \
                 libgrpc++-dev protobuf-compiler-grpc libssl-dev
-
 
 #Clone dotfiles
 git clone https://github.com/robert-ohurley/dotfiles.git $HOME/dotfiles
@@ -71,8 +73,6 @@ continue?
 
 # Ruby
 sudo apt-get install ruby-full
-
-sudo apt instal sshfs
 
 #Zshell
 chsh -s $(which zsh)
@@ -104,17 +104,14 @@ sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
 continue?
 
-
 #I can finally get rid of NVM, use FNM instead and maybe alias it to nvm?
 curl -fsSL https://fnm.vercel.app/install | bash
 fnm install 22.5.1
 continue?
 
-
 #TPM
 git clone https://github.com/tmux-plugins/tpm $XDG_CONFIG_HOME/tmux/plugins/tpm
 continue?
-
 
 #Golang <3
 cd $HOME/Downloads
@@ -122,14 +119,12 @@ curl -OL https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.22.3.linux-amd64.tar.gz
 continue?
 
-
 #Nerd Fonts
 cd $HOME/Downloads
 sudo curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.0/JetBrainsMono.zip
 unzip JetBrainsMono.zip -d $XDG_DATA_HOME/fonts/
 fc-cache $XDG_DATA_HOME/fonts
 continue?
-
 
 # Docker
 sudo apt-get update
@@ -146,7 +141,6 @@ sudo chmod 666 /var/run/docker.sock
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 continue?
 
-
 #Brave Browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
@@ -154,26 +148,21 @@ sudo apt update
 sudo apt install brave-browser -y
 continue?
 
-
 #Stow dotfiles
 cd $HOME/dotfiles/
 stow -v -t $HOME/.config config
 continue?
 
-
 #Additional configuration
 curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
 continue?
-
 
 #Cleanup
 rm $HOME/Downloads/JetBrainsMono.zip
 rm $HOME/Downloads/go1.22.3.linux-amd64.tar.gz
 continue?
 
-
 #Notes to self.
 echo "Remember to install tmux packages <prefix> + I"
 
-
-#Run -> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+git config --global core.excludesfile $HOME/dotfiles/config/git/.gitignore
